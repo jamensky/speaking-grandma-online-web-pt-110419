2ad7a0d35ca90d863730286e49b1b6803b1fa252
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-09 18:45:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,9 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.downcase
     puts "HUH?! SPEAK UP, SONNY!"
-elsif phrase == phrase.upcase
-    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    puts "NO, NOT SINCE 1938!"
+  elsif 
+   puts "I LOVE YOU TOO PUMPKIN!"
  end 
 end 
